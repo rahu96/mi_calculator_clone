b4b9dbceff13c8376f03905e74791cf15287de90
rahu96 <rahulcruise96@gmail.com>
2020-01-23 22:51:51 +0530

    Row2 done

--- lib/calculator_page.dart
+++ lib/calculator_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'presentations/custom_modulo_icons.dart';
 import 'presentations/custom_divide_icons.dart';
+import 'presentations/custom_multiply_icons.dart';
 
 class CalculatorPage extends StatefulWidget {
   @override
@@ -68,6 +69,54 @@ class CalculatorPageState extends State<CalculatorPage> {
                 ),
               ],
             ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                FlatButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {},
+                  child: Text(
+                    '7',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {},
+                  child: Text(
+                    '8',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {},
+                  child: Text(
+                    '9',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {},
+                  child: Icon(CustomMultiply.clear,
+                      size: 25.0, color: Colors.orange[600]),
+                ),
+              ],
+            )
           ],
         ),
       ),
